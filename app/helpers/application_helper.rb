04acd014f3f360a351c7e0cbd2c15e0ea76ed115
Zac Willis <zwillis@relevantinc.com>
2020-05-06 09:33:41 -0600

    Style auth forms, add auth links to nav.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+
+  def login_helper style = ''
+    if !current_user
+      (link_to "Register", new_user_registration_path, class: style) + 
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style) 
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style 
+    end 
+  end
+
   def nav_items
     [
       {
